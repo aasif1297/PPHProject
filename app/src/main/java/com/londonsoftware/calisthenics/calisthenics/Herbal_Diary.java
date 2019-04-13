2ca27cea2d95ac14be8006e3abf1476f1401bb03
SaniaTalib <saniatalib903@gmail.com>
2019-04-14 00:45:00 +0500

    Google signin done......

--- app/src/main/java/com/londonsoftware/calisthenics/calisthenics/Herbal_Diary.java
+++ app/src/main/java/com/londonsoftware/calisthenics/calisthenics/Herbal_Diary.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,11 +14,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.muddzdev.styleabletoastlibrary.StyleableToast;
 
 
@@ -25,10 +34,12 @@ public class Herbal_Diary extends AppCompatActivity {
     ImageView backspace;
     EditText loginemail_text;
     EditText loginpass_text;
-    Button loginbtn;
+    Button loginbtn,google_login_btn;
     TextView register_user_textview;
     ProgressBar loginProgress;
 
+    private static final int RC_SIGN_IN = 1;
+    private GoogleApiClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
 
 
@@ -43,6 +54,7 @@ public class Herbal_Diary extends AppCompatActivity {
         loginbtn = findViewById(R.id.login_button);
         register_user_textview = findViewById(R.id.register_textview);
         loginProgress = findViewById(R.id.login_progressbar);
+        google_login_btn=(Button) findViewById(R.id.btn_google);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -89,6 +101,31 @@ public class Herbal_Diary extends AppCompatActivity {
             }
         });
 
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
+                    {
+                        Toast.makeText(Herbal_Diary.this, "Connection to Google Sign in failed...", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        google_login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                signIn();
+            }
+        });
 
 
 
@@ -106,6 +143,66 @@ public class Herbal_Diary extends AppCompatActivity {
         });
 
 
+    }
+
+
+
+
+    private void signIn()
+    {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN)
+        {
+
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if (result.isSuccess())
+            {
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+                Toast.makeText(this, "Please wait, while we are getting your auth result...", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(this, "Can't get Auth result.", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+       // Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            sendToPrelogin();
+                        }
+                        else
+                        {
+                            //Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            String errorMessage = task.getException().getMessage();
+                            StyleableToast.makeText(Herbal_Diary.this, "Error : " + errorMessage, Toast.LENGTH_SHORT, R.style.mytoast3).show();
+                        }
+                    }
+                });
     }
 
     @Override
